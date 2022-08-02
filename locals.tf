47982f4303eb2701245cd64807b8f6eec3ac8cf9
artpasut <pasut67@gmail.com>
2022-08-02 17:48:26 +0700

    DTPK-207 feature: add variable is_automatic_verify_acms to support whether to automatic verify acms

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   domains_validation_options_list = flatten([for k, v in aws_acm_certificate.this : [for d in v.domain_validation_options : merge(d, { acm_arn = v.arn, key = k })]])
-  domains_validation_options_set  = { for v in local.domains_validation_options_list : v.domain_name => v }
+  domains_validation_options_set  = var.is_automatic_verify_acms ? { for v in local.domains_validation_options_list : v.domain_name => v } : {}
   tags = merge(
     {
       "Terraform" = "true"
